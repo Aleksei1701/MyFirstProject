1fdbb4716421300b48aba3db35d060b082f9a3be
106010088 <kunizinaleksei@gmail.com>
2024-10-30 13:03:20 +0300

    scaner branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Kunizin_branch");
+        System.out.print("Введите число: ");
+        int scaner = new Scanner(System.in).nextInt();
+        for(int i = 1;i <= scaner; i++){
+            for(int j = scaner; j >= 1; j--){
+                if(i * j == scaner){
+                    System.out.println(i + " * " + j);
+                }
+            }
+        }
     }
 }
